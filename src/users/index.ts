bdb2b43fcdc954d21e3b08de31c68b3a0ae27b97
Sorokovskys <Sorokovskys@ukr.net>
2026-07-28 14:22:29 +0300

    fix: export

--- src/users/index.ts
+++ src/users/index.ts
@@ -1,6 +1,7 @@
 export { NewUserSchema, type NewUserPayload } from "./new-user.schema.js";
+
 export {
   type UpdateUserPayload,
   UpdateUserSchema,
 } from "./update-user.schema.js";
-export {type GetUserPayload, GetUserSchema} from './get-user.schema.js';
+export {type GetUserPayload, GetUserPayload } from './get-user.schema.js';
