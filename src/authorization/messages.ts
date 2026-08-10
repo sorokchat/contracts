cc43e73b919ba34dd308c8b6dfce9ba234d66100
Sorokovsky <Sorokovskys@ukr.net>
2026-08-10 20:16:10 +0300

    feature: chanfe unauthenticated message

--- src/authorization/messages.ts
+++ src/authorization/messages.ts
@@ -1,3 +1,3 @@
-export const UNAUTHORIZED: string = "Користувач не має доступу";
+export const UNAUTHORIZED: string = "Користувач не авторизований";
 export const BAD_CREDENTIALS: string = "Не коректні данні для входу";
 export const ACCESS_DENIED: string = "У доступі відмовлено";
