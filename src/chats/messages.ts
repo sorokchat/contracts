eb96cc87ae4bfe1dfd0626ef09a37bfa5c44027a
Sorokovsky <Sorokovskys@ukr.net>
2026-08-11 15:54:45 +0300

    feature: add message if member exists

--- src/chats/messages.ts
+++ src/chats/messages.ts
@@ -5,3 +5,4 @@ export const ROLE_NOT_FOUND: string = "Роль не знайдена";
 export const CHAT_NOT_FOUND: string = "Чат не знайдений";
 export const CAN_NOT_REMOVE_LAST_ADMIN: string =
   "Не можна видалити останього адміна чату";
+export const MEMBER_EXISTS: string = "Користувач вже є учасником чату"
