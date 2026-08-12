5ec45d23563067508463f45104f332495f12ae94
Sorokovsky <Sorokovskys@ukr.net>
2026-08-12 19:23:08 +0300

    fix: openapi reslver

--- src/openapi/nestjs.resolver.ts
+++ src/openapi/nestjs.resolver.ts
@@ -1,22 +1,16 @@
 import { readFile } from "node:fs/promises";
 import { dirname, join } from "node:path";
 import { load } from "js-yaml";
-import { type OpenAPIObject } from "@nestjs/swagger";
 import { fileURLToPath } from "node:url";
 
 const __filename: string = fileURLToPath(import.meta.url);
 const __dirname: string = dirname(__filename);
 
-export async function resolveNestJsOpenApi(
-  serverUrl: string,
-): Promise<OpenAPIObject> {
+export async function resolveNestJsOpenApi<T>(): Promise<T> {
   const yamlFile = await readFile(
     join(__dirname, "..", "..", "openapi.yaml"),
     "utf8",
   );
   const yaml = load(yamlFile);
-  const document = yaml as OpenAPIObject;
-  if (!document.servers) document.servers = [];
-  document.servers.push({ url: serverUrl });
-  return document;
+  return yaml as T;
 }
